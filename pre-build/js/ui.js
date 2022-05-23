c3828f07c70c67740cba9b0a6fdd0e7159625f16
Plush777 <sky11916@naver.com>
2022-05-23 18:05:51 +0900

    브라우저창 작업중

--- pre-build/js/ui.js
+++ pre-build/js/ui.js
@@ -18,4 +18,35 @@ $(function(){
     document.getElementById('ampm').innerHTML = amPm;
     document.getElementById('time').innerHTML = getHours + ':' + getMinutes.toString().padStart(2, '0');
     document.getElementById('date').innerHTML = getYmd;
+
+    $('.windows').on({
+        "click": function(){
+            $('.activeMenu').toggleClass('active');
+            $(this).toggleClass('on');
+        }
+    });
+
+    $(document).on({
+        "click": function (e) {
+            if($(e.target).closest('.activeMenu').length === 0 && $(e.target).closest('.windows').length === 0){
+                $('.activeMenu').removeClass('active');
+                $('.windows').removeClass('on');
+            }
+        }
+    });
+
+    $('.appList li').on({
+        "click": function(e){
+            $(this).toggleClass('hover').siblings().removeClass('hover');
+           
+        }
+    });
+
+    $(document).on({
+        "click": function(e){
+            if($(e.target).closest('.appList li').length === 0){
+                $('.appList li').removeClass('hover');
+            }
+        }
+    })
 });

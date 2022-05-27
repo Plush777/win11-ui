b4e577def543ed63791cf9568b18ac871019fe57
Plush777 <sky11916@naver.com>
2022-05-27 16:44:09 +0900

    작업중

--- pre-build/js/ui.js
+++ pre-build/js/ui.js
@@ -74,6 +74,16 @@ $(function(){
         }
     });
 
+    $('.appList li.vsc').on({
+        "dblclick": function(){
+            $('.browser.vsc').toggleClass('open').removeClass('close');
+            $('.browser').removeClass('save');
+            if($('.browser.vsc').hasClass('open')){
+                $('.taskCenter ul li.vsc').addClass('on');
+            }
+        }
+    })
+
     $('.taskCenter ul li.chrome').on({
         "click": function(){
             $('.browser.chrome').toggleClass('open').removeClass('save');
@@ -113,6 +123,18 @@ $(function(){
         }
     });
 
+    $('.bookMarkList ul li.github').on({
+        "click": function(){
+            $('#chromeFrame').attr('src', 'https://github.com/Plush777/win11-ui');
+        }
+    });
+
+    $('.bookMarkList ul li.mySite01').on({
+        "click": function(){
+            $('#chromeFrame').attr('src', 'https://plush777.github.io/component-css/');
+        }
+    });
+
     $('.btnExp').on({
         "click": function(){
             $('.browser').toggleClass('minilize').css({'top': '0', 'left': '0'});
@@ -134,13 +156,16 @@ $(function(){
         containment: '#App',
         handles: 'n, e, s, w, ne, se, sw, nw',
         alsoResize: '#chromeFrame',
+        minWidth: 500,
         start: function(){
             $(this).css('transition', 'none');
             $('.commonFrame').css('transition', 'none');
+            $('.ui-resizable-e').css('width','50px');
         },
         stop: function(){
             $(this).css('transition', 'all .25s cubic-bezier(.85,.14,.14,.85)');
             $('.commonFrame').css('transition', 'all .25s cubic-bezier(.85,.14,.14,.85)');
+            $('.ui-resizable-e').css('width','7px');
         }
     });
 });

129401ac67f2ee5b589c2ea5419de54096c0df4e
Plush777 <sky11916@naver.com>
2023-01-16 18:23:49 +0900

    작업중

--- pre-build/js/task.js
+++ pre-build/js/task.js
@@ -1,89 +1,33 @@
 $(function () {
-    $('.taskCenter ul li').on({
-        "click": function () {
-            if($(this).siblings('li').hasClass('on')){
-                $(this).siblings('li').removeClass('on').addClass('appSave');
-                return;
-            }
+    $('.taskCenter ul li:not(.windows)').on({
+        "click": function (e) {
+            const _this = $(this);
+            const target = e.target.dataset.value;
 
             function taskClick() {
-                $(this).addClass('on').siblings().removeClass('on');
+                _this.toggleClass('on').siblings('li').removeClass('on');
+
+                if (_this.hasClass('on')) {
+                    $(`#${target}`).addClass('open').removeClass('save');
+                } 
+
+                
             }
-            
+
             taskClick();
-
-            if($('#edge').hasClass('open')){
-                $('#taskEdge').addClass('appSave');
-            } else if($('#chrome').hasClass('open')){
-                $('#taskChrome').addClass('appSave');
-            } else if($('setting').hasClass('open')){
-                $('#taskSetting').addClass('appSave');
-            }
-        }
-    });
-
-    $('#taskChrome').on({
-        "click": function () {
-            $('#chrome').toggleClass('open').removeClass('save');
-            
-            if($('#chrome').hasClass('open')){
-                $(this).addClass('on').removeClass('appSave');
-            } else {
-                $(this).removeClass('on').addClass('appSave');
-            }
-        }
-    });
-
-    $('#taskEdge').on({
-        "click": function () {
-            $('#edge').toggleClass('open').removeClass('save');
-
-            if($('#edge').hasClass('open')){
-                $(this).addClass('on').removeClass('appSave');
-            } else {
-                $(this).removeClass('on').addClass('appSave');
-            }
         }
     });
 
     $(document).on('click', '#taskDiscord', function (){
         $('#discord').toggleClass('open').removeClass('save');
-
-        if($('#discord').hasClass('open')){
-            $('#taskDiscord').addClass('on').removeClass('appSave');
-        } else {
-            $('#taskDiscord').removeClass('on').addClass('appSave');
-        }
     });
 
     $(document).on('click', '#taskVsc', function (){
         $('#vsc').toggleClass('open').removeClass('save');
-
-        if($('#vsc').hasClass('open')){
-            $('#taskVsc').addClass('on').removeClass('appSave');
-        } else {
-            $('#taskVsc').removeClass('on').addClass('appSave');
-        }
     });
 
     $(document).on('click', '#taskNotePad', function (){
         $('#notePad').toggleClass('open').removeClass('save');
-
-        if($('#notePad').hasClass('open')){
-            $('#taskNotePad').addClass('on').removeClass('appSave');
-        } else {
-            $('#taskNotePad').removeClass('on').addClass('appSave');
-        }
-    });
-
-    $(document).on('click', '#taskSetting', function (){
-        $('#setting').toggleClass('open').removeClass('save');
-
-        if($('#setting').hasClass('open')){
-            $('#taskSetting').addClass('on').removeClass('appSave');
-        } else {
-            $('#taskSetting').removeClass('on').addClass('appSave');
-        }
     });
 
     $('.windows').on({
